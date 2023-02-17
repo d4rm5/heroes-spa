927da4b676836ff27302feab2765fa30289104b2
d4rm <dantemartinez06@gmail.com>
2023-02-17 18:42:07 -0300

    First commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
+import { PrivateRoute } from '../heroes/routes/PrivateRoute'
+import { PublicRoute } from '../heroes/routes/PublicRoute'
+
+export const AppRouter = () => {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  )
+}
